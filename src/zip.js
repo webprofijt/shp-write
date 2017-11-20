24213df43ebb3b2ec44820cc9afe1c74bca374d8
Gijs Boekema <gboekema@cyclomedia.com>
2017-11-20 16:03:24 +0100

    Provide optional filename optional filename will be used for all files inside zip file.

--- src/zip.js
+++ src/zip.js
@@ -7,6 +7,7 @@ module.exports = function(gj, options) {
 
     var zip = new JSZip();
     var prj = (options && options.prj) ? options.prj : defaultPrj;
+    options = options || {};
 
     [geojson.point(gj), geojson.pointZ(gj), geojson.line(gj), geojson.lineZ(gj), geojson.polygon(gj), geojson.polygonZ(gj)]
         .forEach(function(l) {
@@ -19,7 +20,18 @@ module.exports = function(gj, options) {
                 // geometries
                 l.geometries,
                 function(err, files) {
-                    var fileName = options && options.types[l.type.toLowerCase()] ? options.types[l.type.toLowerCase()] : l.type;
+                    var fileName;
+                    if (options.filename) {
+                        fileName = options.filename.toLowerCase();
+                    } else if (options.types){
+                        if (options.types[l.type.toLowerCase()]) {
+                            fileName = options.types[l.type.toLowerCase()]
+                        } else {
+                            fileName = l.type.toLowerCase();
+                        }
+                    } else {
+                        fileName = l.type.toLowerCase();
+                    }
                     zip.file(fileName + '.shp', files.shp.buffer, { binary: true });
                     zip.file(fileName + '.shx', files.shx.buffer, { binary: true });
                     zip.file(fileName + '.dbf', files.dbf.buffer, { binary: true });
